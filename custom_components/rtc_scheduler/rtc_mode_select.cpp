1c3358c8b8783185acef6570d6a0a78107b8c5d0
Electrick5 <121640280+Electrick5@users.noreply.github.com>
2024-05-18 23:44:50 +0300

    Update rtc_mode_select.cpp

--- custom_components/rtc_scheduler/rtc_mode_select.cpp
+++ custom_components/rtc_scheduler/rtc_mode_select.cpp
@@ -7,7 +7,7 @@ static const char *TAG = "rtc_scheduler_scheduled_item ";
 //TODO add some more to dump config
  void RTCSchedulerItemMode_Select::dump_config()
 {
-  LOG_SELECT("", "RTC Schedule Item", this);
+  LOG_SELECT("", "Пункт розкладу RTC", this);
  
 } 
 
@@ -18,14 +18,14 @@ void RTCSchedulerItemMode_Select::setup()
   this->pref_ = global_preferences->make_preference<size_t>(this->get_object_id_hash());
   if (!this->pref_.load(&index)) {
     value = "Manual Off";
-    ESP_LOGD(TAG, "State from initial (could not load): %s", value.c_str());
+    ESP_LOGD(TAG, "Початковий стан (не вдалося завантажити): %s", value.c_str());
   } else {
     value = this->traits.get_options().at(index);
-    ESP_LOGD(TAG, "State from restore: %s", value.c_str());
+    ESP_LOGD(TAG, "Стан після відновлення: %s", value.c_str());
   }
   this->publish_state(value);  
 }
-// Setup the scheduled item with its various components
+// Налаштуйте запланований елемент із його різними компонентами
 void RTCSchedulerItemMode_Select::configure_item(uint8_t item_slot_number, RTCSchedulerControllerSwitch *item_sw, switch_::Switch *item_sw_id, RTCSchedulerTextSensor *item_status, RTCSchedulerTextSensor *item_next_event,binary_sensor::BinarySensor *item_on_indicator)
 {
   this->item_slot_number_ = item_slot_number;
@@ -39,21 +39,21 @@ void RTCSchedulerItemMode_Select::configure_item(uint8_t item_slot_number, RTCSc
   }
   this->item_status_ = item_status;
   if (this->item_status_ != nullptr){
-    this->item_status_->publish_state("Initialising");
+    this->item_status_->publish_state("Ініціалізація");
   }
   
   this->item_next_event_ = item_next_event;
   if (this->item_next_event_ != nullptr){
-    this->item_next_event_->publish_state("None");
+    this->item_next_event_->publish_state("Жодного");
   }
 
   this->item_on_indicator_ = item_on_indicator;
   if (this->item_on_indicator_ != nullptr){
     this->item_on_indicator_->publish_state(false);
   }
-//TODO Need to adjust states based on recovered mode and validity and schedule
+//TODO Потрібно налаштувати стани на основі відновленого режиму, терміну дії та розкладу
 }
-// Tell the scheduled item that its controller is either off or on
+// Повідомте запланованому елементу, що його контролер увімкнено або вимкнено
 void RTCSchedulerItemMode_Select::set_controller_state(bool schedule_controller_state)
 {
   this->schedule_controller_state_= schedule_controller_state;
@@ -63,7 +63,7 @@ bool RTCSchedulerItemMode_Select::get_controller_state()
 {
   return this->schedule_controller_state_;
 }
-// Tell the scheduled item that it should turn  off or on by schedule
+// Скажіть запланованому елементу, що він має вимкнути або ввімкнути за розкладом
 void RTCSchedulerItemMode_Select::set_scheduled_item_state(bool scheduled_item_state)
 {
   this->schedule_controller_item_state_ = scheduled_item_state;
@@ -79,14 +79,14 @@ bool RTCSchedulerItemMode_Select::get_scheduled_item_state()
     schedule_valid_ = schedule_valid;
    
  }
-// Depending on the item mode driven by the HA select and the current schedule controller EG (On or Off) Adjust the rest of the internal states 
+// Залежно від режиму елемента, керованого вибором HA та поточного контролера розкладу EG (Увімк. або Вимк.) Налаштуйте решту внутрішніх станів 
 void RTCSchedulerItemMode_Select::adjustItemInternalState()
 {
 // First lets deal with the corner case of a invalid schedule / start-up
   if (not schedule_valid_){
     item_mode_state_ = ITEM_STATE_INIT;
     item_mode_ = ITEM_MODE_OFF;
-    ESP_LOGD(TAG, "adjust Not  Valid");
+    ESP_LOGD(TAG, "Налаштування не дійсні");
   }
   else{
     switch (this->item_mode_)
@@ -114,14 +114,14 @@ void RTCSchedulerItemMode_Select::adjustItemInternalState()
 
   this->prepareItemSensorAndSwitches();
 }
-// Driven by the internal mode state update the switches, indicator and status text
+// Керуючись станом внутрішнього режиму, оновіть перемикачі, індикатор і текст стану
 void RTCSchedulerItemMode_Select::prepareItemSensorAndSwitches()
 {
-   ESP_LOGD(TAG, "entered Prepare");
+   ESP_LOGD(TAG, "приготування введення");
   switch (this->item_mode_state_)
   {
   case ItemModeState::ITEM_STATE_INIT:
-    item_mode_state_str_ = "Not configured";
+    item_mode_state_str_ = "Не сконфігуровано";
     adjustItemSensorAndSwitches(false, item_mode_state_str_);
     break;
   case ItemModeState::ITEM_STATE_OFF:
@@ -191,7 +191,7 @@ uint8_t RTCSchedulerItemMode_Select::get_slot_number()
   return this->item_slot_number_;
 }
 void RTCSchedulerItemMode_Select::adjustStatusTextSensor(std::string &newValue)
-{ //TODO add name to string
+{ //TODO додати назву до рядка
   if (this->item_status_ != nullptr){
     this->item_status_->publish_state(newValue);
   }
@@ -203,7 +203,7 @@ void RTCSchedulerItemMode_Select::adjustEventTextSensor(std::string &newValue)
     this->item_next_event_->publish_state(newValue);
   }
 }
-// Allow the schedule item to display its next event if the text sensor is configured
+// Дозволити елементу розкладу відображати наступну подію, якщо налаштовано текстовий датчик
 void RTCSchedulerItemMode_Select::set_next_scheduled_event(uint16_t next_schedule_event, bool next_schedule_state)
 {
    
@@ -214,7 +214,7 @@ void RTCSchedulerItemMode_Select::set_next_scheduled_event(uint16_t next_schedul
 }
 std::string RTCSchedulerItemMode_Select::convertEventTimeToStr(uint16_t next_schedule_event) const
 {
-  // time is in mins, so need to convert to Day, Hours, Mins
+  // час у хвилинах, тому потрібно конвертувати в день, години, хвилини
   uint8_t days = next_schedule_event/(24*60);
   uint16_t n = next_schedule_event%(24*60);
   uint8_t hours = n / 60;
@@ -251,7 +251,7 @@ std::string RTCSchedulerItemMode_Select::convertEventTimeToStr(uint16_t next_sch
   result = str_sprintf( " %02d:%02d",hours,mins);
 
   result = dayStr +result;
-  ESP_LOGD(TAG, "Converstion %s",result.c_str());  
+  ESP_LOGD(TAG, "Конвертування %s",result.c_str());  
   return result;
 }
 void RTCSchedulerItemMode_Select::control(const std::string &value)
@@ -259,18 +259,18 @@ void RTCSchedulerItemMode_Select::control(const std::string &value)
   convertEventTimeToStr(8639);
   std::string value_Internal = value;
   if (not schedule_valid_){
-    ESP_LOGD(TAG, "Slot %d item, schedule is not valid so stay in Manual Off",this->item_slot_number_);
+    ESP_LOGD(TAG, "Слот %d , розклад не дійсний тому Manual Off",this->item_slot_number_);
     value_Internal = "Manual Off";
     this->state = value_Internal;
   }
   else
-    ESP_LOGD(TAG, "Setting slot %d item mode %s",this->item_slot_number_,value_Internal.c_str());
+    ESP_LOGD(TAG, "Налаштування слота %d режим елемента %s",this->item_slot_number_,value_Internal.c_str());
   this->publish_state(value_Internal);
   // save the state
   auto options = this->traits.get_options();
   size_t index = std::find(options.begin(), options.end(), value_Internal) - options.begin();
   this->pref_.save(&index);
-  ESP_LOGD(TAG, "Saving slot %d index %d",index,index);
+  ESP_LOGD(TAG, "Зберегти слот %d index %d",index,index);
   this->item_mode_ = static_cast<ItemMode>(index);
   adjustItemInternalState();
 }
